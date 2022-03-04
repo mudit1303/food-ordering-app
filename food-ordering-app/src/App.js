837fe76c0a2215a4f13b4b142d59bd48f6a673fb
mudit1303 <80175458+mudit1303@users.noreply.github.com>
2022-03-04 15:28:22 +0530

    home page and footer implemented

--- food-ordering-app/src/App.js
+++ food-ordering-app/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import {BrowserRouter as Router,Switch, Route} from 'react-router-dom' 
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom' 
+import Home from './pages/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <NavBar>
-
-        </NavBar>
+        <NavBar></NavBar>
+        <Routes>
+            <Route path="/" element={<Home></Home>} />
+        </Routes>
+        <Footer></Footer>
       </Router>
     </div>
   );
---
+++ food-ordering-app/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import banner from '../assests/food.jpeg';
+import { Link } from 'react-router-dom';
+import '../styles/home.css';
+function Home() {
+  return (
+    <div className='home' style={{backgroundImage: `url(${banner})`}}  >
+        <div className='headerContainer'>
+            <h1>Food Corner</h1>
+            <p>INDIAN FOOD AT A CLICK</p>
+            <Link to='/menu'>
+                <button>ORDER NOW</button>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Home
